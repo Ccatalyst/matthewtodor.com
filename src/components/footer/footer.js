a16de762fdc5056723b7d9420789509340466c22
Matthew Todor <unclejunglegeorge@gmail.com>
2022-05-08 18:47:00 -0600

    added twitter to footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,18 +1,21 @@
-import React, { useState } from "react";
+import React from "react";
 import "./footer.css";
-import { BsGithub, BsLinkedin, BsMailbox, BsPhone } from "react-icons/bs";
+import { BsGithub, BsLinkedin, BsMailbox, BsPhone, BsTwitter } from "react-icons/bs";
 function Footer() {
 	return (
 		<>
 			<div className="footer">
-				<a href="mailto:todor.matthew.john@gmail.com" target="_blank">
+				<a href="mailto:todor.matthew.john@gmail.com" target="_blank" rel="noreferrer">
 					<BsMailbox />
 				</a>
-				<a href="https://github.com/Ccatalyst" target="_blank">
+				<a href="https://github.com/Ccatalyst" target="_blank" rel="noreferrer">
 					<BsGithub />
 				</a>
-				<a href="https://www.linkedin.com/in/matthew-todor-a9185062/" target="_blank">
+				<a href="https://www.linkedin.com/in/matthew-todor-a9185062/" target="_blank" rel="noreferrer">
 					<BsLinkedin />
+				</a>
+				<a href="https://twitter.com/Ccatalysttt" target="_blank" rel="noreferrer">
+					<BsTwitter />
 				</a>
 				<a>
 					<BsPhone /> 303-489-5742
