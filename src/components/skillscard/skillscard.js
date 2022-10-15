66ee0d1f1f5ebf9fdcbf45e4804ff1b622e2dc44
Matthew Todor <unclejunglegeorge@gmail.com>
2022-10-15 16:37:24 -0600

    Working out icons for resume page

--- src/components/skillscard/skillscard.js
+++ src/components/skillscard/skillscard.js
@@ -3,7 +3,7 @@ import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import { Button, Typography } from "@mui/material";
 import resume from "../../assets/resume/MatthewTodorResume.pdf";
 import "./skillscard.scss";
-
+import { DiHtml5 } from "react-icons/di";
 const SkillsTable = () => {
 	return (
 		<>
@@ -34,64 +34,55 @@ const SkillsTable = () => {
 					</Grid>
 					<Grid container sm={6} borderLeft="2px solid" borderColor="primary.main" pl={2} mt={-2} position="relative" className="resumeitem">
 						<Grid item sm={8} m="auto">
-							<Typography variant="h6" py={0.5} px={1} align="center" gutterBottom sx={{ backgroundColor: "#0E959466" }}>
+							<Typography variant="h6" py={0.5} px={1} align="center" my={{ xs: 2, sm: 0 }} sx={{ backgroundColor: "#0E959466" }}>
 								Full Stack Web Development Teacher's Assistant
 							</Typography>
 						</Grid>
 						<Grid item sm={12} m="auto">
-							<Typography variant="caption" paragraph gutterBottom align="center" fontStyle="oblique">
+							<Typography variant="caption" paragraph gutterBottom align="center" mt={1} fontStyle="oblique">
 								July 2022-present
 							</Typography>
 						</Grid>
-						<Grid item>
+
+						<Grid item component="article">
 							<ul>
-								<Typography variant="body1" component="article" px={1}>
-									<li>
-										Assisted in teaching students of various development skill levels web development concepts and languages, including HTML, CSS and
-										Javascript.
-									</li>
+								<Typography variant="body1" component="li" px={1} sx={{ listStyle: "none" }}>
+									Assisted in teaching students of various development skill levels web development concepts and languages, including HTML, CSS and
+									Javascript.
 								</Typography>
-								<Typography variant="body1" component="article" px={1}>
-									<li>Built upon the foundational languages to teach both front-end and back-end development, all while fully remote.</li>
+								<Typography variant="body1" component="li" px={1} sx={{ listStyle: "none" }}>
+									Built upon the foundational languages to teach both front-end and back-end development, all while fully remote.
 								</Typography>
 							</ul>
 
 							<ul>
-								<Typography variant="body1">
-									<li>Lesson Planning and Grading</li>
+								<Typography variant="body1" component="li" sx={{ listStyle: "none" }}>
+									Lesson Planning and Grading
 								</Typography>
 								<ul>
-									<Typography variant="body2" fontSize={15}>
-										<li>
-											Worked with instructor on reviewing lesson plans for the day, going over goals and potential stumbling blocks, discussing
-											solutions and simplified explanations for complex subjects
-										</li>
+									<Typography variant="body2" component="li" fontSize={15}>
+										Worked with instructor on reviewing lesson plans for the day, going over goals and potential stumbling blocks, discussing
+										solutions and simplified explanations for complex subjects
 									</Typography>
-									<Typography variant="body2" fontSize={15}>
-										<li>
-											Worked with fellow Teacher’s Assistants and Instructor to analyze students’ projects and grade based on functionality,
-											appearance, and best practices.
-										</li>
+									<Typography variant="body2" component="li" fontSize={15}>
+										Worked with fellow Teacher’s Assistants and Instructor to analyze students’ projects and grade based on functionality, appearance,
+										and best practices.
 									</Typography>
 								</ul>
 							</ul>
 
 							<ul>
-								<Typography variant="body1">
-									<li>Code Review</li>
+								<Typography variant="body1" component="li" sx={{ listStyle: "none" }}>
+									Code Review
 								</Typography>
 								<ul>
-									<Typography variant="body2" fontSize={15}>
-										<li>
-											Analyzed code from students to assist in finding the source of bugs, working through the operation of the functions verbally to
-											find the source of the problem
-										</li>
+									<Typography variant="body2" component="li" fontSize={15}>
+										Analyzed code from students to assist in finding the source of bugs, working through the operation of the functions verbally to
+										find the source of the problem
 									</Typography>
-									<Typography variant="body2" fontSize={15}>
-										<li>
-											Pair programed to help students understand various concepts of web development, walking through simple and complex methods of
-											Javascript.
-										</li>
+									<Typography variant="body2" component="li" fontSize={15}>
+										Pair programed to help students understand various concepts of web development, walking through simple and complex methods of
+										Javascript.
 									</Typography>
 								</ul>
 							</ul>
@@ -114,86 +105,132 @@ const SkillsTable = () => {
 						position="relative"
 						className="resumeitem"
 					>
-						<Grid item sm={6} m="auto">
-							<Typography variant="h6" py={0.5} px={1} align="center" gutterBottom sx={{ backgroundColor: "#0E959466" }}>
+						<Grid item sm={8} m="auto">
+							<Typography variant="h6" py={0.5} px={1} align="center" my={{ xs: 2, sm: 0 }} sx={{ backgroundColor: "#0E959466" }}>
 								Golf
 							</Typography>
 						</Grid>
 						<Grid item component="article">
 							<ul>
-								<Typography variant="body1" fontSize={15} px={1}>
-									<li>Consistent focus on personal improvement, with a focus on repetition and routine.</li>
+								<Typography variant="body1" component="li" fontSize={15} px={1} sx={{ listStyle: "none" }} gutterBottom>
+									Consistent focus on personal improvement, with a focus on repetition and routine.
 								</Typography>
-								<Typography variant="body1" fontSize={15} px={1}>
-									<li>A frequent review of recent performance increases learning speed and personal performance.</li>
+								<Typography variant="body1" component="li" fontSize={15} px={1} sx={{ listStyle: "none" }} gutterBottom>
+									A frequent review of recent performance increases learning speed and personal performance.
 								</Typography>
 							</ul>
 						</Grid>
 					</Grid>
 					<Grid container sm={6} borderLeft="2px solid" borderColor="primary.main" pl={1} mt={-2} position="relative" className="resumeitem">
-						<Grid item sm={6} m="auto">
-							<Typography variant="h6" py={0.5} px={1} align="center" gutterBottom sx={{ backgroundColor: "#0E959466" }}>
+						<Grid item sm={8} m="auto">
+							<Typography variant="h6" py={0.5} px={1} align="center" my={{ xs: 2, sm: 0 }} sx={{ backgroundColor: "#0E959466" }}>
 								World of Warcraft
 							</Typography>
 						</Grid>
 						<Grid item component="article">
 							<ul>
-								<Typography variant="body1" fontSize={15} gutterBottom px={1}>
-									<li>
-										Weekly scheduled collaboration with between 19-24 other people, coordinating movement and actions in a precise manner to
-										accomplish our goals.
-									</li>
+								<Typography variant="body1" component="li" fontSize={15} gutterBottom px={1} sx={{ listStyle: "none" }}>
+									Weekly scheduled collaboration with between 19-24 other people, coordinating movement and actions in a precise manner to accomplish
+									our goals.
 								</Typography>
-								<Typography variant="body1" fontSize={15} gutterBottom px={1}>
-									<li>Quick corrections when mistakes are made to lower the time spent on any single challenge</li>
+								<Typography variant="body1" component="li" fontSize={15} gutterBottom px={1} sx={{ listStyle: "none" }}>
+									Quick corrections when mistakes are made to lower the time spent on any single challenge
 								</Typography>
-								<Typography variant="body1" fontSize={15} gutterBottom px={1}>
-									<li>
-										Requires communication across a variety of mediums to keep up to date on the status of various goals and what contributors can do
-										to help accomplish the next endeavor
-									</li>
+								<Typography variant="body1" component="li" fontSize={15} gutterBottom px={1} sx={{ listStyle: "none" }}>
+									Requires communication across a variety of mediums to keep up to date on the status of various goals and what contributors can do to
+									help accomplish the next endeavor
 								</Typography>
 							</ul>
 						</Grid>
 					</Grid>
 				</Grid>
 
-				<Grid container xs={12} sm={6} p={1} justifyContent="center">
+				<Grid container xs={12} sm={6} p={1} px={{ xs: 3 }} justifyContent="center">
 					<Grid item xs={12}>
 						<Typography variant="h4" gutterBottom color="primary" align="center" py={4}>
 							Skillset
 						</Typography>
 					</Grid>
-					<Grid item>
-						<ul>
-							<h3>Languages</h3>
-							<li>Javascript</li>
-							<li>HTML</li>
-							<li>CSS</li>
-						</ul>
-
-						<ul>
-							<h3>Tools</h3>
-							<li>Node</li>
-							<li>Express</li>
-							<li>SQL</li>
-							<li>MongoDB</li>
-							<li>React</li>
-							<li>Handlebars</li>
-							<li>GraphQL</li>
-							<li>Google</li>
-						</ul>
-
-						<ul>
-							<h3>Applications</h3>
-							<li>Github</li>
-							<li>Mongo Compass</li>
-							<li>Insomnia</li>
-							<li>Apollo Sandbox</li>
-							<li>MySQL</li>
-							<li>Heroku</li>
-						</ul>
+					<Grid container sm={6} borderLeft="2px solid" borderColor="primary.main" pl={2} mt={-2} ml={1} position="relative" className="resumeitem">
+						<Grid item sm={8} m="auto">
+							<Typography variant="h6" py={0.5} px={1} align="center" my={{ xs: 2, sm: 0 }} minWidth={110} sx={{ backgroundColor: "#0E959466" }}>
+								Languages
+							</Typography>
+						</Grid>
+						<Grid item sm={6} component="article" m="auto">
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								HTML
+							</Typography>
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								CSS
+							</Typography>
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								Javascript
+							</Typography>
+						</Grid>
 					</Grid>
+					<Grid container sm={6} borderLeft="2px solid" borderColor="primary.main" pl={2} mt={-2} ml={1} position="relative" className="resumeitem">
+						<Grid item sm={8} m="auto">
+							<Typography variant="h6" py={0.5} px={1} align="center" my={{ xs: 2, sm: 0 }} minWidth={110} sx={{ backgroundColor: "#0E959466" }}>
+								Tools
+							</Typography>
+						</Grid>
+						<Grid item sm={8} component="article">
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								Nodejs
+							</Typography>
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								Express
+							</Typography>
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								SQL
+							</Typography>
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								MongoDB
+							</Typography>
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								React
+							</Typography>
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								Handlebars
+							</Typography>
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								GraphQL
+							</Typography>
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								Google
+							</Typography>
+						</Grid>
+					</Grid>
+
+					<Grid container sm={6} borderLeft="2px solid" borderColor="primary.main" pl={2} mt={-2} ml={1} position="relative" className="resumeitem">
+						<Grid item xs={8} m="auto">
+							<Typography variant="h6" py={0.5} px={1} align="center" my={{ xs: 2, sm: 0 }} minWidth={130} sx={{ backgroundColor: "#0E959466" }}>
+								Applications
+							</Typography>
+						</Grid>
+						<Grid item component="article">
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								Github
+							</Typography>
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								Mongo Compass
+							</Typography>
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								Insomnia
+							</Typography>
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								Apollo Sandbox
+							</Typography>
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								MySQL
+							</Typography>
+							<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+								Heroku
+							</Typography>
+						</Grid>
+					</Grid>
+
 					<Grid item xs={12}>
 						<Typography variant="h4" gutterBottom color="primary" align="center" py={4}>
 							Certificates
