81040f62dca56fb57653e438175b235dc1e1d716
Matthew Todor <unclejunglegeorge@gmail.com>
2022-10-15 00:19:52 -0600

    added more information to the resume page, as well as a button on the bottom of the page that mirrors the one at the top. More work needs to be done on it, namely on the skillset and certificates section. Added a blurb on the algorithms page as a 'check back later' sort of message.

--- src/components/pages/algorithms.js
+++ src/components/pages/algorithms.js
@@ -5,9 +5,18 @@ import { Typography } from "@mui/material";
 const Algorithms = () => {
 	return (
 		<Grid container>
-			<Grid item>
-				<Typography variant="h1" component="h6">
+			<Grid item xs={12} m="auto">
+				<Typography variant="h2" component="h6" align="center" color="primary">
 					Algorithms
+				</Typography>
+			</Grid>
+			<Grid item xs={12} m="auto" backgroundColor="background.paper" maxWidth="xl">
+				<Typography variant="subtitle1" paragraph align="center">
+					This space is designed to be used for the algorithms I have solved through the full stack bootcamp I help teach and participated in, as well
+					as the CodeWars algorithms I've done since.
+				</Typography>
+				<Typography variant="subtitle1" paragraph align="center">
+					While at the moment it is empty, it will be quickly filled as I continue to work on this site.
 				</Typography>
 			</Grid>
 		</Grid>
--- src/components/skillscard/skillscard.js
+++ src/components/skillscard/skillscard.js
@@ -28,21 +28,33 @@ const SkillsTable = () => {
 				{/* EXPERIENCE */}
 				<Grid container xs={12} sm={6} p={1} justifyContent="center">
 					<Grid item xs={12}>
-						<Typography variant="h5" gutterBottom color="primary" align="center">
+						<Typography variant="h4" gutterBottom color="primary" align="center" py={4}>
 							Experience
 						</Typography>
 					</Grid>
-					<Grid container sm={6} borderLeft="2px solid" borderColor="primary.main" pl={1} position="relative" className="resumeitem">
-						<Grid item sm={6} m="auto">
+					<Grid container sm={6} borderLeft="2px solid" borderColor="primary.main" pl={1} mt={-2} position="relative" className="resumeitem">
+						<Grid item sm={8} m="auto">
 							<Typography variant="h6" py={0.5} px={1} align="center" gutterBottom sx={{ backgroundColor: "#0E959466" }}>
 								Full Stack Web Development Teacher's Assistant
 							</Typography>
 						</Grid>
-						<Grid item>
-							<Typography variant="body1" component="article" px={1}>
-								Assisted in teaching students of various development skill levels web development concepts and languages, including HTML, CSS and
-								Javascript. Built upon the foundational languages to teach both front-end and back-end development, all while fully remote.
+						<Grid item sm={12} m="auto">
+							<Typography variant="caption" paragraph gutterBottom align="center" fontStyle="oblique">
+								July 2022-present
 							</Typography>
+						</Grid>
+						<Grid item>
+							<ul>
+								<Typography variant="body1" component="article" px={1}>
+									<li>
+										Assisted in teaching students of various development skill levels web development concepts and languages, including HTML, CSS and
+										Javascript.
+									</li>
+								</Typography>
+								<Typography variant="body1" component="article" px={1}>
+									<li>Built upon the foundational languages to teach both front-end and back-end development, all while fully remote.</li>
+								</Typography>
+							</ul>
 
 							<ul>
 								<Typography variant="body1">
@@ -87,117 +99,114 @@ const SkillsTable = () => {
 					</Grid>
 					{/* EXTRACURRICULAR ACTIVITIES */}
 					<Grid item xs={12}>
-						<Typography variant="h5" gutterBottom color="primary" align="center">
+						<Typography variant="h4" gutterBottom color="primary" align="center" py={4}>
 							Extracurricular Activities
 						</Typography>
 					</Grid>
-					<Grid container sm={6} borderLeft="2px solid" borderColor="primary.main" pl={1} m={1} position="relative" className="resumeitem">
-						<Grid item sm={7} m="auto">
-							<Typography variant="h6" py={0.5} px={1} align="center" gutterBottom sx={{ backgroundColor: "#0E959466" }}>
-								Golf
-							</Typography>
-						</Grid>
-						<Grid item>
-							<ul>
-								<Typography variant="body1" component="article" px={1}>
-									<li>Consistent focus on personal improvement, with a focus on repetition and routine.</li>
-								</Typography>
-								<Typography variant="body1" component="article" px={1}>
-									<li>A frequent review of recent performance increases learning speed and personal performance.</li>
-								</Typography>
-							</ul>
-						</Grid>
-					</Grid>
-					<Grid container sm={6} borderLeft="2px solid" borderColor="primary.main" pl={1} m={1} position="relative" className="resumeitem">
+					<Grid container sm={6} borderLeft="2px solid" borderColor="primary.main" pl={1} m={1} mt={-2} position="relative" className="resumeitem">
 						<Grid item sm={6} m="auto">
 							<Typography variant="h6" py={0.5} px={1} align="center" gutterBottom sx={{ backgroundColor: "#0E959466" }}>
 								Golf
 							</Typography>
 						</Grid>
-						<Grid container sm={6}>
-							<Typography variant="body1" component="article" px={1}>
-								Consistent focus on personal improvement, with a focus on repetition and routine. A frequent review of recent performance increases
-								learning speed and personal performance.
+						<Grid item component="article">
+							<ul>
+								<Typography variant="body1" fontSize={15} px={1}>
+									<li>Consistent focus on personal improvement, with a focus on repetition and routine.</li>
+								</Typography>
+								<Typography variant="body1" fontSize={15} px={1}>
+									<li>A frequent review of recent performance increases learning speed and personal performance.</li>
+								</Typography>
+							</ul>
+						</Grid>
+					</Grid>
+					<Grid container sm={6} borderLeft="2px solid" borderColor="primary.main" pl={1} mt={-2} position="relative" className="resumeitem">
+						<Grid item sm={6} m="auto">
+							<Typography variant="h6" py={0.5} px={1} align="center" gutterBottom sx={{ backgroundColor: "#0E959466" }}>
+								World of Warcraft
 							</Typography>
+						</Grid>
+						<Grid item component="article">
+							<ul>
+								<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+									<li>
+										Weekly scheduled collaboration with between 19-24 other people, coordinating movement and actions in a precise manner to
+										accomplish our goals.
+									</li>
+								</Typography>
+								<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+									<li>Quick corrections when mistakes are made to lower the time spent on any single challenge</li>
+								</Typography>
+								<Typography variant="body1" fontSize={15} gutterBottom px={1}>
+									<li>
+										Requires communication across a variety of mediums to keep up to date on the status of various goals and what contributors can do
+										to help accomplish the next endeavor
+									</li>
+								</Typography>
+							</ul>
 						</Grid>
 					</Grid>
 				</Grid>
 
-				<Grid container xs={12} sm={6} justifyContent="center">
+				<Grid container xs={12} sm={6} p={1} justifyContent="center">
 					<Grid item xs={12}>
-						<Typography variant="h5" gutterBottom color="primary" align="center">
+						<Typography variant="h4" gutterBottom color="primary" align="center" py={4}>
 							Skillset
 						</Typography>
 					</Grid>
 					<Grid item>
-						<div className="skillList">
-							<ul>
-								<h3>Languages</h3>
-								<li>Javascript</li>
-								<li>HTML</li>
-								<li>CSS</li>
-							</ul>
-						</div>
-						<div className="skillList">
-							<ul>
-								<h3>Tools</h3>
-								<li>Node</li>
-								<li>Express</li>
-								<li>SQL</li>
-								<li>MongoDB</li>
-								<li>React</li>
-								<li>Handlebars</li>
-								<li>GraphQL</li>
-								<li>Google</li>
-							</ul>
-						</div>
-						<div className="skillList">
-							<ul>
-								<h3>Applications</h3>
-								<li>Github</li>
-								<li>Mongo Compass</li>
-								<li>Insomnia</li>
-								<li>Apollo Sandbox</li>
-								<li>MySQL</li>
-								<li>Heroku</li>
-							</ul>
-						</div>
+						<ul>
+							<h3>Languages</h3>
+							<li>Javascript</li>
+							<li>HTML</li>
+							<li>CSS</li>
+						</ul>
+
+						<ul>
+							<h3>Tools</h3>
+							<li>Node</li>
+							<li>Express</li>
+							<li>SQL</li>
+							<li>MongoDB</li>
+							<li>React</li>
+							<li>Handlebars</li>
+							<li>GraphQL</li>
+							<li>Google</li>
+						</ul>
+
+						<ul>
+							<h3>Applications</h3>
+							<li>Github</li>
+							<li>Mongo Compass</li>
+							<li>Insomnia</li>
+							<li>Apollo Sandbox</li>
+							<li>MySQL</li>
+							<li>Heroku</li>
+						</ul>
+					</Grid>
+					<Grid item xs={12}>
+						<Typography variant="h4" gutterBottom color="primary" align="center" py={4}>
+							Certificates
+						</Typography>
 					</Grid>
 				</Grid>
-				{/* <h4>Achievements</h4>
+				<Grid container xs={12} my={2}>
+					{/* Add the same link animation to this that you have on the nav bar */}
 
-				<div className="skillList">
-					<ul>
-						<h3>Languages</h3>
-						<li>Javascript</li>
-						<li>HTML</li>
-						<li>CSS</li>
-					</ul>
-				</div>
-				<div className="skillList">
-					<ul>
-						<h3>Tools</h3>
-						<li>Node</li>
-						<li>Express</li>
-						<li>SQL</li>
-						<li>MongoDB</li>
-						<li>React</li>
-						<li>Handlebars</li>
-						<li>GraphQL</li>
-						<li>Google</li>
-					</ul>
-				</div>
-				<div className="skillList">
-					<ul>
-						<h3>Applications</h3>
-						<li>Github</li>
-						<li>Mongo Compass</li>
-						<li>Insomnia</li>
-						<li>Apollo Sandbox</li>
-						<li>MySQL</li>
-						<li>Heroku</li>
-					</ul>
-				</div> */}
+					<Button
+						size="large"
+						variant="outlined"
+						href={resume}
+						target="_blank"
+						rel="noreferrer"
+						sx={{ m: "auto", borderRadius: `${1}px` }}
+						className="resumedlbutton"
+					>
+						<Typography variant="body1" component="p">
+							Download Resume
+						</Typography>
+					</Button>
+				</Grid>
 			</Grid>
 		</>
 	);
