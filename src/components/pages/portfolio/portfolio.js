960657f2efad36841bf838ba5298f3d963ca5917
Matthew Todor <unclejunglegeorge@gmail.com>
2022-10-15 23:46:38 -0600

    removed unused var

--- src/components/pages/portfolio/portfolio.js
+++ src/components/pages/portfolio/portfolio.js
@@ -1,22 +1,10 @@
 import React from "react";
-import { Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import projects from "./projects.json";
 import PortfolioCard from "./portfoliocard";
 const Portfolio = () => {
 	return (
 		<Grid container>
-			<Grid item xs={12} sx={{ mt: 1 }}>
-				<Typography
-					variant="h4"
-					component="h2"
-					align="left"
-					color="primary"
-					sx={{ backgroundColor: "#12121200", textDecoration: "underline", ml: 4 }}
-				>
-					Here is some of the work I've done
-				</Typography>
-			</Grid>
 			{projects.map((project, index) => {
 				return <PortfolioCard key={index} props={{ project }} />;
 			})}
