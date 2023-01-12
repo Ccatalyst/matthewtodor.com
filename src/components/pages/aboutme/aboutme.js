50e204a4e196c8f91c0b22cabf917137a2e6ce84
Matthew Todor <unclejunglegeorge@gmail.com>
2023-01-12 12:04:10 -0700

    added looping to aboutme typewriter component

--- src/components/pages/aboutme/aboutme.js
+++ src/components/pages/aboutme/aboutme.js
@@ -15,13 +15,7 @@ const AboutMe = () => {
 			<Grid item xs={12}>
 				<Box elevation={0} sx={{ width: 95 / 100, m: "auto" }}>
 					<Typography variant="h4" component="h3" sx={{ m: 1, p: 1, color: "primary.main" }} align="center" fontFamily="Raleway">
-						<Typewriter
-							options={{ strings: ["A Programmer", "A Teacher", "Full Stack"], autoStart: true, delay: 75, pauseFor: 3600 }}
-							// sequence={[1000, "A Programmer", 3000, "A Teacher", 3000, "Full Stack", 3000]}
-							// wrapper="div"
-							// cursor={true}
-							// repeat={Infinity}
-						/>
+						<Typewriter options={{ strings: ["A Programmer", "A Teacher", "Full Stack"], autoStart: true, loop: true, delay: 75, pauseFor: 3600 }} />
 					</Typography>
 				</Box>
 			</Grid>
