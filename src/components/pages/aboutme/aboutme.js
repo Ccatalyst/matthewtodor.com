3811d27d71e60bf428b6503e4c192bb45e5b03d4
Matthew Todor <unclejunglegeorge@gmail.com>
2023-01-12 12:01:21 -0700

    forgot about the type-animation component in aboutme.js. Replaced with typewriter

--- src/components/pages/aboutme/aboutme.js
+++ src/components/pages/aboutme/aboutme.js
@@ -2,7 +2,7 @@ import React from "react";
 import { Typography, Box } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
 import "./aboutme.css";
-import { TypeAnimation } from "react-type-animation";
+import Typewriter from "typewriter-effect";
 
 const AboutMe = () => {
 	return (
@@ -15,11 +15,12 @@ const AboutMe = () => {
 			<Grid item xs={12}>
 				<Box elevation={0} sx={{ width: 95 / 100, m: "auto" }}>
 					<Typography variant="h4" component="h3" sx={{ m: 1, p: 1, color: "primary.main" }} align="center" fontFamily="Raleway">
-						<TypeAnimation
-							sequence={[1000, "A Programmer", 3000, "A Teacher", 3000, "Full Stack", 3000]}
-							wrapper="div"
-							cursor={true}
-							repeat={Infinity}
+						<Typewriter
+							options={{ strings: ["A Programmer", "A Teacher", "Full Stack"], autoStart: true, delay: 75, pauseFor: 3600 }}
+							// sequence={[1000, "A Programmer", 3000, "A Teacher", 3000, "Full Stack", 3000]}
+							// wrapper="div"
+							// cursor={true}
+							// repeat={Infinity}
 						/>
 					</Typography>
 				</Box>
