76f95f371cf56c07fbc62c38e51006681b5b9d02
Matthew Todor <unclejunglegeorge@gmail.com>
2023-01-28 00:30:50 -0700

    added code for emailjs. Need to sort out how to pass formData state as 3rd parameter

--- src/components/pages/contact/contact.tsx
+++ src/components/pages/contact/contact.tsx
@@ -1,25 +1,7 @@
 import React from "react";
 import { Button, TextField, Typography } from "@mui/material";
 import Grid from "@mui/material/Unstable_Grid2/Grid2";
-
-// const reasons: Array<Reasons> = [
-// 	{
-// 		value: "Regarding an open position",
-// 		label: "Regarding an open position",
-// 	},
-// 	{
-// 		value: "Regarding collaborating on a project",
-// 		label: "Regarding collaborating on a project",
-// 	},
-// 	{
-// 		value: "Curious about a current project",
-// 		label: "Curious about a current project",
-// 	},
-// 	{
-// 		value: "Other",
-// 		label: "Other (specify in details)",
-// 	},
-// ];
+import emailjs from "@emailjs/browser";
 
 type Document = {
 	name: string;
@@ -46,7 +28,17 @@ const Contact = (): JSX.Element => {
 			[name]: value,
 		}));
 	};
-	console.log(formData);
+
+	const sendEmail = (e: React.FormEvent) => {
+		e.preventDefault();
+
+		emailjs
+			.sendForm(process.env.SERVICE_ID as string, process.env.TEMPLATE_ID as string, e.target as HTMLFormElement, process.env.PUBLIC_KEY as string)
+			.then((result) => {
+				console.log(result.text);
+			});
+	};
+
 	return (
 		<Grid container component="main" maxWidth="lg" mx="auto" sx={{ backgroundColor: "#12121290" }}>
 			<Grid xs={12} mt={3}>
@@ -62,80 +54,86 @@ const Contact = (): JSX.Element => {
 					Contact Me
 				</Typography>
 			</Grid>
-			<Grid mt={3} xs={11} md={5} mx="auto">
-				<TextField
-					required
-					id="name"
-					label="Name"
-					helperText="Required"
-					variant="standard"
-					onChange={handleChange}
-					fullWidth
-					color="primary"
-					InputLabelProps={{ shrink: !!formData.name }}
-					value={formData.name}
-				/>
-			</Grid>
-			<Grid mt={3} xs={11} md={5} mx="auto">
-				<TextField
-					required
-					id="email"
-					label="Email"
-					helperText="Required"
-					variant="standard"
-					onChange={handleChange}
-					fullWidth
-					InputLabelProps={{ shrink: !!formData.email }}
-					value={formData.email}
-				/>
-			</Grid>
-			<Grid mt={3} xs={11} md={5} mx="auto">
-				<TextField
-					id="company"
-					label="Company"
-					helperText=""
-					variant="standard"
-					onChange={handleChange}
-					fullWidth
-					InputLabelProps={{ shrink: !!formData.company }}
-					value={formData.company}
-				/>
-			</Grid>
-			<Grid mt={3} xs={11} md={5} mx="auto">
-				<TextField
-					required
-					id="reason"
-					label="Reason"
-					helperText="Why are you reaching out?"
-					onChange={handleChange}
-					variant="standard"
-					fullWidth
-					InputLabelProps={{ shrink: !!formData.reason }}
-					value={formData.reason}
-				></TextField>
-			</Grid>
-			<Grid mt={3} xs={11} md={10} mx={{ xs: "auto" }}>
-				<TextField
-					required
-					id="details"
-					label="Details"
-					helperText="Required"
-					variant="standard"
-					onChange={handleChange}
-					fullWidth
-					multiline
-					rows={3}
-					InputLabelProps={{ shrink: !!formData.details }}
-					value={formData.details}
-				/>
-			</Grid>
-
-			<Grid mt={3} xs={11} md={10} height={15} mx={{ xs: "auto" }}></Grid>
-
-			<Grid container xs={12} my={2} justifyContent="center">
-				<Button type="submit" size="large" variant="outlined" sx={{ borderRadius: `${1}px`, px: 6, py: 1, backgroundColor: "#00000090" }}>
-					Send
-				</Button>
+			<Grid container component="form" xs={12} mt={3} onSubmit={sendEmail}>
+				<Grid mt={3} xs={11} md={5} mx="auto">
+					<TextField
+						required
+						id="name"
+						label="Name"
+						helperText="Required"
+						variant="standard"
+						onChange={handleChange}
+						fullWidth
+						color="primary"
+						InputLabelProps={{ shrink: !!formData.name }}
+						value={formData.name}
+					/>
+				</Grid>
+				<Grid mt={3} xs={11} md={5} mx="auto">
+					<TextField
+						required
+						id="email"
+						label="Email"
+						helperText="Required"
+						variant="standard"
+						onChange={handleChange}
+						fullWidth
+						InputLabelProps={{ shrink: !!formData.email }}
+						value={formData.email}
+					/>
+				</Grid>
+				<Grid mt={3} xs={11} md={5} mx="auto">
+					<TextField
+						id="company"
+						label="Company"
+						helperText=""
+						variant="standard"
+						onChange={handleChange}
+						fullWidth
+						InputLabelProps={{ shrink: !!formData.company }}
+						value={formData.company}
+					/>
+				</Grid>
+				<Grid mt={3} xs={11} md={5} mx="auto">
+					<TextField
+						required
+						id="reason"
+						label="Reason"
+						helperText="Why are you reaching out?"
+						onChange={handleChange}
+						variant="standard"
+						fullWidth
+						InputLabelProps={{ shrink: !!formData.reason }}
+						value={formData.reason}
+					></TextField>
+				</Grid>
+				<Grid mt={3} xs={11} md={10} mx={{ xs: "auto" }}>
+					<TextField
+						required
+						id="details"
+						label="Details"
+						helperText="Required"
+						variant="standard"
+						onChange={handleChange}
+						fullWidth
+						multiline
+						rows={3}
+						InputLabelProps={{ shrink: !!formData.details }}
+						value={formData.details}
+					/>
+				</Grid>
+				<Grid mt={3} xs={11} md={10} height={15} mx={{ xs: "auto" }}></Grid>
+				<Grid container xs={12} my={2} justifyContent="center">
+					<Button
+						type="submit"
+						size="large"
+						variant="outlined"
+						sx={{ borderRadius: `${1}px`, px: 6, py: 1, backgroundColor: "#00000090" }}
+						onClick={sendEmail}
+					>
+						Send
+					</Button>
+				</Grid>
 			</Grid>
 		</Grid>
 	);
